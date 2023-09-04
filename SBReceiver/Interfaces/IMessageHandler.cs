f007a806494f9ab94d13b6ebeac2370025d339c4
Farrag Abuelela <farrag_software@hotmail.com>
2023-09-04 15:55:46 +0300

    add IMessageHandler, Processor, Serializer add BaseMessageHandler, Processor, Serializer

---
+++ SBReceiver/Interfaces/IMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SBReceiver.Interfaces
+{
+    public interface IMessageHandler
+    {
+        bool CanBeHandledType(Type type);
+        void Invoke(object messsage);
+    }
+    public interface IMessageHandler<T>
+    {
+        void Invoke(T messsage);
+    }
+}
---
+++ SBReceiver/Services/BaseMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using SBReceiver.Interfaces;
+using SBShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SBReceiver.Services
+{
+    public abstract class BaseMessageHandler <T> : IMessageHandler, IMessageHandler<T> where T : class
+    {
+        public bool CanBeHandledType(Type type)
+        {
+            return type == typeof(IMessage<>);
+        }
+
+        public void Invoke(T messsage) { }
+
+
+        public void Invoke(object messsage) { }
+    }
+}
